ea882d142124845cd79a3a3f00cfa8b0435afdfb
Yerkebulan Sharipov <94905139+Yorkie10@users.noreply.github.com>
2023-10-04 12:02:41 +0600

    Initial Commit

---
+++ WordsProjectV2/WordsProjectV2App.swift
@@ -0,0 +1,17 @@
+//
+//  WordsProjectV2App.swift
+//  WordsProjectV2
+//
+//  Created by Yerkebulan Sharipov on 04.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct WordsProjectV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
